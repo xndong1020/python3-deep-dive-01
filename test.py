aeb3d747a0eb2ca06b133963f1f6a99245d2fdd5
JGU <jgu@networkten.com.au>
2020-01-18 18:15:55 +1100

    init setup

---
+++ test.py
@@ -0,0 +1,22 @@
+class Rectangle:
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+
+    @property # getter method to read value from field _width
+    def width(self):
+        return self._width
+
+    @width.setter # getter method to encapsulate field _width
+    def width(self, value):
+        if value <= 0:
+           raise ValueError('Width must be positive')
+        else:
+           self._width = value
+
+    def area(self):
+        return self.width * self.height
+         
+
+
+r1 = Rectangle(-100, 20)
